4867155a4604229f86c193ebefc70748fca30797
Shuhao Sun <Shuhao@ShuhaodeMacBook-Pro.local>
2018-04-27 22:04:32 -0700

    Finish process request

--- server.cpp
+++ server.cpp
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <strings.h>
 #include <signal.h>
+#include <dirent.h>
 #include <regex>
 #include <iterator>
 #include <iostream>
@@ -48,6 +49,28 @@ void Server::error(const char *msg)
     perror(msg);
     exit(1);
 }
+
+void Server::send_404() 
+{
+    //construct the 404 header
+    write(newsockfd, "HTTP/1.1 404 Not Found\n", 14);
+    write(newsockfd, "Content-Length: 13\n", 19);
+    write(newsockfd, "Content-Type: text/html\n\n", 25);
+    write(newsockfd, "<h1>404 Not Found</h1>", 23);
+    close(newsockfd);
+}
+
+void convert_to_lowercase(char *str)
+{
+    while (*str) 
+    {
+        *str = tolower(*str);
+        str++;
+    }
+    return;
+}
+
+
 
 void Server::create_socket()
 {
@@ -91,11 +114,10 @@ void Server::server_listen()
         {
             error("ERROR reading from socket");
         }
-
+        //process the client request message
+        printf("%s\n", buffer);
         std::string filename = parser(buffer);
-        printf("%s\n", filename.c_str());
-
-
+        //printf("%s\n",filename.c_str());
         process_request(filename);
         // close connection
         close(newsockfd); 
@@ -109,7 +131,13 @@ const std::string Server::parser(const std::string &rqst)
     // std::cout << rqst;
     std::size_t idx_high = rqst.find("HTTP/1.1");
 
-    std::string filename = rqst.substr(idx_low+1, idx_high-idx_low-2);
+    std::string filename;
+    if (idx_high-2 <= idx_low+1)
+    {
+        filename = ":no_file_name";
+        return filename;
+    }
+    filename = rqst.substr(idx_low+1, idx_high-idx_low-2);
     // replace all %20 with " "
 
     std::size_t idx_space = filename.find("%20");
@@ -123,57 +151,8 @@ const std::string Server::parser(const std::string &rqst)
 
 }
 
-
-
-void Server::process_request(const std::string &filename)
+const std::string Server::get_contentType(const std::string &filename)
 {
-    
-    // find extension to build header
-    std::string Content_Type = get_contentType(filename);
-
-    // file stream
-    std::ifstream inFile;
-    inFile.open("./"+filename, std::ifstream::in | std::ios::binary);
-    if(!inFile) {
-        send_404();
-        return;
-    }
-    char c;
-
-    std::string response; 
-    while(inFile.get(c)) {
-        response.push_back(c);
-    }
-
-    
-
-    // send back
-    if(inFile.eof()) {
-        std::string response_header = "HTTP/1.1 200 OK\n";
-
-        response_header+=Content_Type; //file_type_respnose, application/octet-stream for random stuff
-        response_header+="\n";
-        response_header+="Content-Length: ";
-        response_header+=std::to_string(response.size());
-        response_header+="\n";
-        response_header+="\n";
-
-        write(newsockfd, response_header.c_str(), response_header.size());
-        write(newsockfd, response.c_str(), response.size());
-        return;
-
-        return;
-
-    } else {
-        send_404();
-        return;
-    }
-
-
-
-}
-const std::string Server::get_contentType(const std::string &filename){
-
     std::size_t idx_dot = filename.find_last_of(".");
     std::string extension;
     // find the pos of extension
@@ -187,30 +166,91 @@ const std::string Server::get_contentType(const std::string &filename){
     std::string Content_Type;
 
     // define different content type header base on file extension
-    if ( (strcasecmp(extension.c_str(), "html") == 0)){
-        Content_Type = "Content-Type: text/html\n\n";
-    }else if( (strcasecmp(extension.c_str(), "jpg") == 0) || (strcasecmp(extension.c_str(), "jpeg") == 0)){
-        Content_Type = "Content-Type: image/jpeg\n\n";
+    if ( (strcasecmp(extension.c_str(), "html") == 0) || 
+        (strcasecmp(extension.c_str(), "htm") == 0) ||
+        (strcasecmp(extension.c_str(), "txt") == 0)){
+        Content_Type = "Content-Type: text/html\n";
+    }else if( (strcasecmp(extension.c_str(), "jpg") == 0) || 
+        (strcasecmp(extension.c_str(), "jpeg") == 0)){
+        Content_Type = "Content-Type: image/jpeg\n";
     }else if( (strcasecmp(extension.c_str(), "gif") == 0)){
-        Content_Type = "Content-Type: image/gif\n\n";
+        Content_Type = "Content-Type: image/gif\n";
     }else{
         Content_Type = "Content-Type: application/octet-stream\n\n";
     }
 
     // printf("%s\n",Content_Type.c_str());
     return Content_Type;
-
 }
 
+void Server::process_request(const std::string &filename)
+{
+    if (filename.compare(":no_file_name") == 0)
+    {
+        send_404();
+        return;
+    }
+    // find extension to build header
+    std::string Content_Type = get_contentType(filename);
 
-void Server::send_404() {
-    //construct the 404 header
-    write(newsockfd, "HTTP/1.1 404 Not Found\n", 14);
-    write(newsockfd, "Content-Length: 13\n", 19);
-    write(newsockfd, "Content-Type: text/html\n\n", 25);
-    write(newsockfd, "<h1>404 Not Found</h1>", 23);
-    close(newsockfd);
+    //check directory to find the file to open :flnm
+    std::string flnm = "";
+    DIR *dp;
+    struct dirent *ep;
+    dp = opendir(".");
+    if (dp == NULL)
+    {
+        perror ("Couldn't open the directory");
+        return;
+    }
+    while((ep = readdir(dp)))
+    {
+        char* s1 = strdup(ep->d_name);
+        char* s2 = strdup(filename.c_str());
+        convert_to_lowercase(s1);
+        convert_to_lowercase(s2);
+        if(strcmp(s1, s2) == 0)
+        {
+            flnm = ep->d_name;
+        }
+        free(s1);
+        free(s2);
+    }
+
+    // read binary file to form entity body
+    if(flnm == "")
+    {
+        send_404();
+        return;
+    }
+    std::ifstream ifs(flnm, std::ifstream::in | std::ios::binary | std::ios::ate);
+    if(!ifs) 
+    {
+        send_404();
+        return;
+    }
+    std::streampos ifs_size = ifs.tellg();
+    char *entity_body = new char[ifs_size];
+    ifs.seekg(0, std::ios::beg);
+    ifs.read(entity_body, ifs_size);
+    ifs.close();
+
+    // create headerlines
+    std::string header_lines; 
+    header_lines += "HTTP/1.1 200 OK\n";
+    header_lines += "Connection: Keep-Alive\n";
+    header_lines += "Content-Length: ";
+    header_lines += std::to_string(ifs_size);
+    header_lines += Content_Type;
+    header_lines += "\n";
+
+    //send response message
+    write(newsockfd, header_lines.c_str(), header_lines.size());
+    write(newsockfd, entity_body, ifs_size);
+    delete[] entity_body;
+    return;
 }
+
 
 Server* server = NULL;
 
