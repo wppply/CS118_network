69a49c70a10fd5e8003c7bdb58c368601e5f45fc
wppply <wppply@gmail.com>
2018-06-06 20:50:35 -0700

    correct server udp socket programming and handshake

--- project2/server.cpp
+++ project2/server.cpp
@@ -7,95 +7,92 @@
 #include <netdb.h>
 #include <unistd.h>
 #include "tcp.h"
-
 #include <arpa/inet.h>
 
-void error(char *msg)
-{
-    perror(msg);
-    exit(1);
-}
+typedef struct pkt_t pkt_t;
 
 class Server
 {
 	public:
-		Server(char *host_name, int port_number);
+		Server(int port_number);
 		~Server();
+		void setup_server();
 		void hand_shake();
 	private:
-		int sockfd, newsockfd, portno
+		int sockfd, newsockfd, portno;
 		unsigned long cli_seq_num, serv_seq_num;
 		socklen_t clilen;
-		char *hostname;
 		struct sockaddr_in serv_addr, cli_addr;
-		bool connection = false;
+		bool connection;
+};
+
+Server::Server(int port_number)
+{
+	portno = port_number;
+	serv_seq_num = 0;
+	connection = false;
 }
 
-Server::Server(char *host_name, int port_number)
-{
-	hostname = host_name;
-	portno = port_number;
-	ser_seq_num = 0;
-}
 void Server::setup_server() 
 {
 
-	if (argc < 3) 
-	{
-		fprintf(stderr,"ERROR: Usage: ./sev [hostname] [port]");
-		exit(1);
-	}
-	
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);  // create socket
-	if (sockfd < 0)
-        error("ERROR opening socket");
+	// Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
     // option to prevent ocuppied socket
     // int setsockopt(int sockfd, int level, int optname, const void *optval, socklen_t optlen);
 
-    //
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    memset((char *) &cli_addr, 0, sizeof(cli_addr));
+
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(hostname)//INADDR_ANY;// redefine the ip host here
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
 
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
 
-    clilen = sizeof(cli_addr);
-
+    // clilen = sizeof(cli_addr);
 
 }
 void Server::hand_shake()
 {
-	listen(sockfd, 5);  // 5 simultaneous connection at most
 
-	
+	// receiving request from server
+	pkt_t recv_req;
+	socklen_t len;
 
-	char buffer[8193]; //buffer 大小怎么定？
-	memset(buffer, 0, 8193);
+    int recvlen = recvfrom(sockfd, &recv_req, sizeof(pkt_t), 0, (struct sockaddr *) &serv_addr, &len);
+    if (recvlen == -1)
+        error("Error: fail to receive package");
 
-	// int n = recvfrom(sockfd, buffer, 8193, 0, (struct sockaddr *) &cli_addr, &clilen);// pos mean receive data
+    serv_seq_num = recv_req.seq_num;
+    cli_seq_num = recv_req.ack_num;
 
-	struct pkt_t clientPacket;
-	buffer2pkt(&clientPacket, buffer);
+	if (recv_req.SYN && connection == false) 
+	{
+		// ack the syn
+      	pkt_t syn_ack;
+      	cli_seq_num = recv_req.ack_num + 1;
+      	serv_seq_num = recv_req.seq_num;
+	  	make_pkt(&syn_ack, true, false, false, serv_seq_num, cli_seq_num, 0, NULL);
 
-	if (clientPacket.SYN && connection == false) {
-      struct pkt_t syn_ack_pkt;
+	    if (sendto(sockfd, &syn_ack, sizeof(pkt_t), 0, (struct sockaddr *) &cli_addr, clilen) < 0) 
+		    printf("Error writing SYN-ACK packet\n"); 
+	    //receive ack
+	    pkt_t rev_ack;
+    	if (recvfrom(sockfd, &rev_ack, sizeof(pkt_t), 0, (struct sockaddr *) &serv_addr, &len) < 0)
+        	error("Error: fail to receive package");
+        serv_seq_num = rev_ack.seq_num;
+    	cli_seq_num = rev_ack.ack_num;
+        connection = true;
 
-      syn_ack_pkt.seq_num = 0; 
-      syn_ack_pkt.SYN = true;
-      syn_ack_pkt.ack_num = clientPacket.ack_num + 1; 
-      if (sendto(sock_fd, &syn_ack_pkt, sizeof(struct pkt_t), 0, 
-      			(struct sockaddr *) &client_addr, cli_len) > 0) 
-      {
-            printf("Sending packet %d SYN\n", (syn_ack_pkt.seq_num));
-            connection = true;
-      }
-      else {
-        printf("Error writing SYN-ACK packet\n"); 
-      }
     }
+
 
 }
 
@@ -105,16 +102,17 @@ int main(int argc, char *argv[])
 {
 	if (argc < 3) 
 	{
-		fprintf(stderr,"ERROR: Usage: ./sev [hostname] [port]");
+		fprintf(stderr,"ERROR: Usage: ./sev [port]");
 		exit(1);
 	}
 
-	char *hostname = argv[1];
-    int portno = atoi(argv[2]);
+    int portno = atoi(argv[1]);
 
-    Server *server = new Server(hostname, portno);
-    server.setup_server();
-    server.hand_shake();
+    Server *server = new Server(portno);
+    server->setup_server();
+    server->hand_shake();
+
+
 
 
 
