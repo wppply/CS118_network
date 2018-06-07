6f18bb358ce037fd6b45da45908083451a72fa27
wppply <wppply@gmail.com>
2018-06-07 13:04:14 -0700

    update handshake and init send file

--- project2/server.cpp
+++ project2/server.cpp
@@ -9,7 +9,6 @@
 #include "tcp.h"
 #include <arpa/inet.h>
 
-typedef struct pkt_t pkt_t;
 
 class Server
 {
@@ -18,6 +17,10 @@ class Server
 		~Server();
 		void setup_server();
 		void hand_shake();
+        void send_packet(pkt_t *packet);
+        void recv_packet(pkt_t *packet);
+        void fill_pkt(pkt_t *packet, FILE *file, int len);
+        void send_file();
 	private:
 		int sockfd, newsockfd, portno;
 		unsigned long cli_seq_num, serv_seq_num;
@@ -32,6 +35,46 @@ Server::Server(int port_number)
 	serv_seq_num = 0;
 	connection = false;
 }
+
+void Server::send_packet(pkt_t *packet)
+{
+    int sendlen = sendto(sockfd, (char *) packet, sizeof(pkt_t), 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    if (sendlen == -1)
+        error("Error: fail to send package");
+}
+
+void Server::recv_packet(pkt_t *packet)
+{
+    socklen_t len;
+    int recvlen = recvfrom(sockfd, packet, sizeof(pkt_t), 0, (struct sockaddr *) &serv_addr, &len);
+    if (recvlen == -1)
+        error("Error: fail to receive package");
+}
+
+void Server::fill_pkt(pkt_t *packet, FILE *file, int len)
+{
+
+}
+
+void Server::send_file() 
+{   
+
+
+
+    FILE *file = fopen(file_name, "rb");
+    if (file == NULL) 
+    {
+        error("failed to open file. file not exist")
+    }
+
+    struct stat stat_buf;
+    int file_size = fstat(fd, &stat_buf);
+    file_size == 0 ? stat_buf.st_size : -1;
+
+
+}
+
+
 
 void Server::setup_server() 
 {
@@ -77,19 +120,14 @@ void Server::hand_shake()
 	{
 		// ack the syn
       	pkt_t syn_ack;
-      	cli_seq_num = recv_req.ack_num + 1;
-      	serv_seq_num = recv_req.seq_num;
-	  	make_pkt(&syn_ack, true, false, false, serv_seq_num, cli_seq_num, 0, NULL);
-
-	    if (sendto(sockfd, &syn_ack, sizeof(pkt_t), 0, (struct sockaddr *) &cli_addr, clilen) < 0) 
-		    printf("Error writing SYN-ACK packet\n"); 
+	  	make_pkt(&syn_ack, true, false, false, serv_seq_num, ++cli_seq_num, 0, NULL);
+        send_packet(syn_ack)
 	    //receive ack
-	    pkt_t rev_ack;
-    	if (recvfrom(sockfd, &rev_ack, sizeof(pkt_t), 0, (struct sockaddr *) &serv_addr, &len) < 0)
-        	error("Error: fail to receive package");
-        serv_seq_num = rev_ack.seq_num;
-    	cli_seq_num = rev_ack.ack_num;
+	    pkt_t rec_ack;
+    	recv_packet(&rec_ack)
         connection = true;
+        printf("server: waiting request from client \n");
+        // update seq_nums 
 
     }
 
