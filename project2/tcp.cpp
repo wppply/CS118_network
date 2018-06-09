308e009b936c10fcaa796fb30ff4d6c2902a7406
wppply <wppply@gmail.com>
2018-06-08 18:14:31 -0700

    update on tcp

--- project2/tcp.cpp
+++ project2/tcp.cpp
@@ -10,8 +10,18 @@ void error(const char *msg)
 }
 
 
-static unsigned long cal_check_sum(unsigned char *str, int size)
+static unsigned long cal_check_sum(pkt_t *packet, int size)
     {
+        unsigned char str[MAX_DATASIZE + 6];
+        str[0] = (unsigned char) packet->SYN;
+        str[1] = (unsigned char) packet->ACK;
+        str[2] = (unsigned char) packet->FIN;
+        str[3] = (unsigned char) packet->seq_num;
+        str[4] = (unsigned char) packet->ack_num;
+        str[5] = (unsigned char) packet->file_status;
+        str[6] = (unsigned char) packet->size;
+        strncat(str, packet->data, sizeof(str));
+        
         unsigned long hash = 5381;
         for(int c = 0; c < size; c++)
             hash = ((hash << 5) + hash) + str[c]; /* hash * 33 + c */
@@ -25,27 +35,22 @@ void make_pkt(pkt_t *packet, bool SYN, bool ACK, bool FIN, short seq_num,
     packet->ACK = ACK;
     packet->FIN = FIN;
     packet->seq_num = seq_num;
+    packet->ack_num = ack_num;
     packet->file_status = file_status;
     packet->data_size = size;
-    if (size == 0)
-        return;
-    if (data != NULL)
-    {
-        packet->check_sum = cal_check_sum((unsigned char *)data, size);
-        bcopy(data, packet->data, size);
-    }
-    else 
-    {
-        fprintf(stderr, "%s\n", "Error: Data is NULL while size not equal zero");
-        exit(1);
-    }
+
+    bzero(packet->data, size);
+    memcpy(packet->data, data, size);
+    
+    packet->check_sum = cal_check_sum(packet, size);
+
 }
 
 bool check_pkt (pkt_t *packet)
 {
     if (packet->data_size == 0)
         return true;
-    unsigned long computed_cs = cal_check_sum((unsigned char*) packet->data, packet->data_size);
+    unsigned long computed_cs = cal_check_sum(packet, packet->data_size);
     return computed_cs == packet->check_sum;
 }
 
